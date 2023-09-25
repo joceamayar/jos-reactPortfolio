2abada723ad75dd36e8ccba9dc075c925a18f44f
joceamayar <jocelin@tavlee.com>
2023-09-25 00:33:07 -0400

    created a single project component then use it in the portfolio page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,32 @@
+
 function Projects() {
-    return (
-      <h1>Projects</h1>
-    );
-  }
-  
-  export default Projects;
-  
+
+  return (
+<div>
+      <div className="card" style={{ width: '18rem' }}>
+        <img className="card-img-top" src="src/assets/eventPulse.png" alt="EventPulse website image" />
+        <div className="card-body">
+          <h5 className="card-title">EventPulse</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="https://event-plannerkas-1ae2bc64599a.herokuapp.com/" className="btn btn-primary">view project</a>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: '18rem' }}>
+        <img className="card-img-top" src="src/assets/nebulaNarratives.png" alt="NebularNarratives website image" />
+        <div className="card-body">
+          <h5 className="card-title">NebulaNarratives</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="https://cdziedzic.github.io/Nebula-Narratives/" className="btn btn-primary">View project</a>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default Projects;
