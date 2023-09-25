edfab9c3b2b49528479370d40c0935199afb0557
joceamayar <jocelin@tavlee.com>
2023-09-25 02:30:02 -0400

    added avatar, and text to about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,28 @@
 function About() {
   return (
-    <div>
-    <img src="./avatar.jpeg" alt="..." class="img-thumbnail"></img>
+    <section id="intro-container">
+            <div className="about-me">
+                <h2 id="intro-h3">Jocelin Amaya</h2>
+                <img src="/src/assets/avatar.jpeg" alt="A woman in a business suit" class="avatar"></img>
+                <p id="intro-paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, alias ipsa
+                    rerum
+                    assumenda veniam temporibus ullam magnam molestiae, error, repellat eum accusamus voluptatem placeat
+                    quidem consectetur ipsam possimus nemo. Qui!
+                    Laboriosam ipsa sapiente quisquam, temporibus iusto totam consectetur fuga quibusdam! Laborum odit
+                    quaerat sint impedit, dolores at, eius quibusdam deleniti vel, adipisci nulla molestiae sit
+                    perspiciatis nihil asperiores modi voluptatibus?
+                    Facilis alias ab ipsum recusandae perspiciatis sapiente dolores voluptates omnis fuga, optio ex,
+                    rerum veritatis deleniti id reprehenderit repellat nulla aliquid esse fugiat culpa inventore. Rerum
+                    tempore a voluptas laborum.
+                    Architecto aliquid voluptas, reiciendis accusamus reprehenderit optio expedita corrupti sit
+                    blanditiis animi. Fugiat adipisci, quos quibusdam quis sint vitae voluptas autem facere natus,
+                    expedita officiis eos, fugit sit. Id, recusandae?
+                    Nostrum iure consectetur in asperiores provident. Corrupti vitae dolorum, dolore libero voluptate
+                    commodi doloremque aliquam quas eligendi error delectus nostrum totam laboriosam iusto inventore,
+                    earum ex numquam doloribus rem. Laboriosam!</p>
+            </div>
 
-
-    <h1>About Page</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-      Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-      mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-      lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-      in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-    </p>
-  </div>
+        </section>
   );
 }
 
